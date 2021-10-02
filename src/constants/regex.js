92fccf093feade2a33d9b9c340727772d29c26f6
Promise <me@promise.solutions>
2021-10-02 17:48:57 +0200

    Remove code to start from scratch again

--- src/constants/regex.js
+++
@@ -1,3 +0,0 @@
-module.exports = {
-  idResolver: /[0-9]+/
-};
--- src/handlers/dutyPing.js
+++
@@ -1,7 +0,0 @@
-const { emojis: { ids: { weewoo }} } = require("../constants");
-
-module.exports.exec = async (message, sod) => {
-  await sod.setMentionable(false, `Role was pinged in #${message.channel.name}`);
-  await message.react(weewoo);
-  setTimeout(() => sod.setMentionable(true), 120000);
-};
